e6433d70006766c3fbcaef0b86a607a06dd2af69
Rahmasamy <rahmasamy949@gmail.com>
2024-11-11 21:03:31 +0200

    first commit

---
+++ app/Models/Booking.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'service_schedule_id',
+        'status',
+    ];
+
+    /**
+     * Get the service schedule associated with this booking.
+     */
+    public function serviceSchedule()
+    {
+        return $this->belongsTo(ServiceSchedule::class, 'service_schedule_id');
+    }
+
+    /**
+     * Get the client associated with this booking.
+     */
+    // public function client()
+    // {
+    //     return $this->belongsTo(Client::class, 'client_id');
+    // }
+}
---
+++ database/migrations/2024_11_11_183240_create_serivce_provider_portfolios_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('serivce_provider_portfolios', function (Blueprint $table) {
+            $table->id();
+            // $table->foreignId('service_provider_id')->constrained()->onDelete('cascade');  // Foreign key to service_providers table
+            $table->text('website_url'); 
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('serivce_provider_portfolios');
+    }
+};
