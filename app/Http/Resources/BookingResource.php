e3808eed1c6143f38c34d6775e2c7dc5b5741e07
Abdulrhman Ahmed <tknwtknwlwjy10@gmail.com>
2024-12-10 13:45:51 +0200

    tst

--- app/Http/Resources/BookingResource.php
+++ app/Http/Resources/BookingResource.php
@@ -48,6 +48,6 @@ class BookingResource extends JsonResource
 
     public function getLeftTime()
     {
-        return now()->diffInMinutes($this->date);
+        return now()->diffInMinutes($this->date->subDay());
     }
 }
