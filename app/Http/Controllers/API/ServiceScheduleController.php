d043e0a800cf5864ecf5fd45df8b1c66eedab6a9
Abdulrhman Ahmed <tknwtknwlwjy10@gmail.com>
2024-12-22 15:37:01 +0200

    test

--- app/Http/Controllers/API/ServiceScheduleController.php
+++ app/Http/Controllers/API/ServiceScheduleController.php
@@ -54,12 +54,12 @@ class ServiceScheduleController extends Controller
                     ->where('end_date', '>=', $date);
             })->latest();
 
-        $schedule = $query->with(['excludedDates', 'customWorkDates.times'])
-            ->with('customDates as customs', function ($q) use ($date) {
-                $q->where('start_date', '<=', $date)
-                    ->where('end_date', '>=', $date)
-                    ->limit(1);
-            })->first();
+        $schedule = $query->with(['excludedDates', 'customWorkDates.times', 'customDates as customs' => function ($q) use ($date) {
+            $q->where('start_date', '<=', $date)
+                ->where('end_date', '>=', $date)
+                ->limit(1);
+        }])
+            ->first();
 
 
         if ($schedule) {
