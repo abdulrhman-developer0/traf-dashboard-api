d5547d936bfdf51bd195eaa409ba4667eb2deed5
Abdulrhman Ahmed <tknwtknwlwjy10@gmail.com>
2024-12-22 16:50:34 +0200

    test

--- app/Http/Controllers/API/ServiceScheduleController.php
+++ app/Http/Controllers/API/ServiceScheduleController.php
@@ -65,8 +65,8 @@ class ServiceScheduleController extends Controller
                 ->count() > 0;
 
             $customQuery = $schedule->customWorkDates()
-                ->where('start_date', '<=', $date)
-                ->where('end_date', '>=', $date)
+                ->where('start_date', '>=', $date->stsartOfDay())
+                ->where('end_date', '<=', $date->endOfDay())
                 ->limit(1);
 
             $schedule['is_custom']  = (bool) $customQuery->count() > 0;
