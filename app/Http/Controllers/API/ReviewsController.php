cf12cff9b997920574bbb550a8a5ff04164bf8f7
Abdulrhman Ahmed <tknwtknwlwjy10@gmail.com>
2024-12-22 18:40:24 +0200

    test

--- app/Http/Controllers/API/ReviewsController.php
+++ app/Http/Controllers/API/ReviewsController.php
@@ -85,7 +85,7 @@ class ReviewsController extends Controller
         if (
             $booking->client->user->id != $user->id && $booking->service->serviceProvider->user->id != $user->id
         ) {
-            return $this->badResponse([], "You not have a booking with id {$request->booking_id}");
+            // return $this->badResponse([], "You not have a booking with id {$request->booking_id}");
         }
 
 
@@ -102,7 +102,9 @@ class ReviewsController extends Controller
         ]);
 
         return [
-            $ratableAccount
+            'u' => $user,
+            'a' => $account,
+            'ra' => $ratableAccount
         ];
 
         $booking->service->update([
