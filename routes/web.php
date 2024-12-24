723e4de0d2b5bf753295160417765085bdf4057c
Abdulrhman Ahmed <tknwtknwlwjy10@gmail.com>
2024-12-24 18:48:41 +0200

    add bookings index

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\RolesController;
 use App\Http\Controllers\PermissionsController;
 use App\Http\Controllers\SystemLogController;
 use App\Http\Controllers\ActivityLogController;
+use App\Http\Controllers\Dashboard\BookingController;
 use App\Http\Controllers\Dashboard\CategoryController;
 use App\Http\Controllers\Dashboard\ClientController;
 use App\Http\Controllers\Dashboard\HomeController;
@@ -23,7 +24,8 @@ use App\Http\Controllers\SystemTrashController;
 // Route::get('/test', [ClientController::class, 'index']);
 // Route::get('/test', [PricingController::class, 'index']);
 // Route::get('/test', [ServiceController::class, 'index']);
-Route::get('/test', [CategoryController::class, 'index']);
+Route::get('/test', [BookingController::class, 'index']);
+// Route::get('/test', [CategoryController::class, 'index']);
 
 Route::get('/', function () {
     return redirect('/dashboard');
