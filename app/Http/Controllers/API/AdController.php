25e992c5a730f66e814557cda92d9d5ea215ace5
Abdulrhman Ahmed <tknwtknwlwjy10@gmail.com>
2024-12-30 17:55:25 +0200

    show approved ads only in home page

--- app/Http/Controllers/API/AdController.php
+++ app/Http/Controllers/API/AdController.php
@@ -25,7 +25,7 @@ class AdController extends Controller
     public function index()
     {
         $ads = Ad::query()
-            // ->whereStatus('approved')
+            ->whereStatus('approved')
             ->latest()
             ->limit(10)
             ->get()
