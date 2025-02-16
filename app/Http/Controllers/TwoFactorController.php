de751debe57a917f5b7b5053aa022dfd41a2ed04
Abdulrhman Ahmed <tknwtknwlwjy10@gmail.com>
2025-02-16 15:52:49 +0200

    refactor the verify code endpoint to send sms

--- app/Http/Controllers/TwoFactorController.php
+++ app/Http/Controllers/TwoFactorController.php
@@ -3,63 +3,68 @@
 namespace App\Http\Controllers;
 
 use App\Notifications\TwoFactorNotification;
-use Carbon\Carbon; 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log; 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Services\SmsService;
+use App\Traits\APIResponses;
 use Twilio\Rest\Client;
 
 class TwoFactorController extends Controller
 {
-    //
+  use APIResponses;
 
-    public function store(Request $request)
-    {
-       $user=Auth::user();
-       \Log::info('Authenticated User:', ['user' => $user]);
-       $user->generateCode();
+  public function __construct(
+    protected SmsService $sms
+  ) {
+    // 
+  }
 
-
-
-       //send mail 
-       $user->notify(new TwoFactorNotification());
-
+  public function store(Request $request)
+  {
+    $user = $request->user();
 
 
-       // send mobile
-    //    $message="رمز التحقق هو ".$user->code;
-    //    $account_sid=getenv("TWILIO_SID");
-    //    $auth_token=getenv("TWILIO_TOKEN");
-    //    $twilio_number=getenv("TWILIO_FROM");
-    //    $client=new Client($account_sid,$auth_token);
-    //    $client->messages->create('+2001027629534',[
-    //     'from'=> $twilio_number,
-    //     'body' => $message
-    //    ]);
-       return response()->json([
-        'message' => 'Verification code sent',
-        'code' => $user->code,  // Remove this in production
+    $user->generateCode();
+
+
+
+    try {
+      //send mail 
+      //  $user->notify(new TwoFactorNotification());
+
+
+      $this->sms->send(
+        $user->phone,
+        "كود التحقق الخاص بك هو $user->code"
+      );
+    } catch (\Throwable $throwable) {
+      // 
+    }
+
+
+    return response()->json([
+      'message' => 'Verification code sent',
+      'code' => $user->code,  // Remove this in production
     ]);
+  }
+  public function show(Request $request)
+  {
+
+    $request->validate([
+      'code' => 'required|numeric'
+    ]);
+    $user = User::whereCode($request->code)->first();
+
+    if ($user->code !== $request->code || Carbon::parse($user->expire_at)->isPast()) {
+      return response()->json(['message' => 'Invalid or expired code'], 403);
     }
-    public function show(Request $request)
-    {
-     
-       $request->validate([
-        'code'=>'required|numeric'
-       ]);
-       $user=User::whereCode($request->code)->first();
 
-       if ($user->code !== $request->code || Carbon::parse($user->expire_at)->isPast()) {
-        return response()->json(['message' => 'Invalid or expired code'], 403);
-    }
+    $user->code_verified = true;
+    $user->save();
 
-      $user->code_verified = true;
-      $user->save();
-
-      return response()->json(['message' => 'Two-factor authentication successful']);
-
-
-    }
- 
+    return response()->json(['message' => 'Two-factor authentication successful']);
+  }
 }
