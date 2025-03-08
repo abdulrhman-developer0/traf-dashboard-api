7bf7adc545efad58c529081272458338948f6822
Abdulrhman Ahmed <tknwtknwlwjy10@gmail.com>
2025-03-08 03:25:08 +0200

    test

--- app/Http/Resources/Dashboard/LatestBookingsCollection.php
+++ app/Http/Resources/Dashboard/LatestBookingsCollection.php
@@ -18,14 +18,16 @@ class LatestBookingsCollection extends ResourceCollection
         $lastPage    = $this->lastPage();
 
         $nextPage = $currentPage + 1;
+        $prevPage = $currentPage - 1;
 
         return [
             'current_page'  => $currentPage,
             'last_page'     => $lastPage,
-            'next_page'     => $nextPage > $lastPage? null : $nextPage,
+            'next_page'     => $nextPage > $lastPage ? null : $nextPage,
+            'prev_page'     => $prevPage < 1 ? null : $prevPage,
             'next_page_url' => $this->nextPageUrl(),
             'per_page'  => $this->perPage(),
-            'items'     => $this->collection->map(function($item) {
+            'items'     => $this->collection->map(function ($item) {
                 return [
                     'id'                => $item->id,
                     'provider_name'     => $item->service?->serviceProvider?->user?->name,
