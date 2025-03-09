ba701d7cdb357fc6436f0d533bc646c69a5cc5d9
Abdulrhman Ahmed <tknwtknwlwjy10@gmail.com>
2025-03-09 02:36:18 +0200

    add pagination to part of dashboard

--- app/Http/Resources/Dashboard/LatestClientCollection.php
+++ app/Http/Resources/Dashboard/LatestClientCollection.php
@@ -14,10 +14,25 @@ class LatestClientCollection extends ResourceCollection
      */
     public function toArray(Request $request): array
     {
+        $currentPage = $this->currentPage();
+        $lastPage    = $this->lastPage();
+
+        $nextPage = $currentPage + 1;
+        $prevPage = $currentPage - 1;
+
+        $currentPage = $this->currentPage();
+        $lastPage    = $this->lastPage();
+
+        $nextPage = $currentPage + 1;
+        $prevPage = $currentPage - 1;
+
         return [
-            'current_page'  => $this->currentPage(),
-            'last_page'     => $this->lastPage(),
+            'current_page'  => $currentPage,
+            'last_page'     => $lastPage,
+            'next_page'     => $nextPage > $lastPage ? null : $nextPage,
+            'prev_page'     => $prevPage < 1 ? null : $prevPage,
             'next_page_url' => $this->nextPageUrl(),
+            'per_page'  => $this->perPage(),
             'items'     => $this->collection->map(function ($item) {
                 return [
                     'id'                => $item->id,
