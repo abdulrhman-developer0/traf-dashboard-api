658eb93f9470840e43c35e9d660ceaa7c2f325ce
Abdulrhman Ahmed <tknwtknwlwjy10@gmail.com>
2025-03-21 23:58:11 +0200

    test

--- app/Http/Controllers/API/Dashboard/JoinRequestController.php
+++ app/Http/Controllers/API/Dashboard/JoinRequestController.php
@@ -63,7 +63,6 @@ class JoinRequestController extends Controller
             return $actual->count();
         })->toArray();
 
-        dd(ServiceProvider::where('status', 'pending')->get()->toArray());
         $providers_paginated = ServiceProvider::query()
             ->whereHas('user', fn($q) => $q->whereNull('deleted_at'))
             ->select(['id', 'user_id', 'tax_registeration_number', 'is_personal', 'status', 'created_at'])
