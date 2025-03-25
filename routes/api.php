7846b550015f81a44672e61b8855395abb117a2d
Abdulrhman Ahmed <tknwtknwlwjy10@gmail.com>
2025-03-25 16:49:15 +0200

    test

--- routes/api.php
+++ routes/api.php
@@ -69,7 +69,7 @@ Route::prefix('auth')->group(function () {
 
         Route::get('/profile/reports', [ProfileController::class, 'reports']);
 
-        Route::patch('/profile/change-photo', [ProfileController::class, 'changePhoto']);
+        Route::any('/profile/change-photo', [ProfileController::class, 'changePhoto']);
         Route::get('/profile/{id}', [ProfileController::class, 'dataFor']);
 
         Route::patch('/profile/change-password', [ProfileController::class, 'changePassword']);
